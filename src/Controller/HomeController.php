b3142ccb3cdeddf01fdf6d1487fa7230a00de110
itearo <itearo@gmail.com>
2020-06-11 19:38:55 +0500

    Добавил robots.txt

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -15,5 +16,21 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    /**
+     * @Route(path="/robots.txt", methods={"get"})
+     * @return Response
+     */
+    public function robots(): Response
+    {
+        return new Response(
+            <<<TXT
+            User-agent: *
+            Disallow: /
+            TXT,
+            Response::HTTP_OK,
+            ['Content-Type' => 'text/plain']
+        );
     }
 }
