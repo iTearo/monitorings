944c83e402969247fde10d35dac5ee88bd624ad5
Ilya Tearo <itearo@gmail.com>
2020-09-09 18:37:29 +0500

    Почистил phinx.php

--- db/phinx/phinx.php
+++ db/phinx/phinx.php
@@ -2,33 +2,17 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-function parseAgnosticDsn(string $dsn): ?array
-{
-    $regex = '#^(?P<adapter>[^\\:]+)\\://(?:(?P<user>[^\\:@]+)(?:\\:(?P<pass>[^@]*))?@)?'
-        . '(?P<host>[^\\:@/]+)(?:\\:(?P<port>[1-9]\\d*))?/(?P<name>[^\?]+)(?:\?(?P<query>.*))?$#';
-
-    if (preg_match($regex, trim($dsn), $parsedOptions)) {
-        $additionalOpts = [];
-        if (isset($parsedOptions['query'])) {
-            parse_str($parsedOptions['query'], $additionalOpts);
-        }
-        $validOptions = ['adapter', 'user', 'pass', 'host', 'port', 'name'];
-        $parsedOptions = array_filter(array_intersect_key($parsedOptions, array_flip($validOptions)));
-        return array_merge($additionalOpts, $parsedOptions);
-    }
-
-    return null;
-}
-
 function makeConfig(string $envFilePath, bool $overrideByEnvs = false): ?array
 {
     $envs = (new Dotenv())->parse(file_get_contents($envFilePath), $envFilePath);
 
     if ($overrideByEnvs === true) {
-        $envs = array_filter($envs, fn($envKey) => getenv($envKey) === false, ARRAY_FILTER_USE_KEY);
+        $envs = array_filter($envs, static fn($envKey) => getenv($envKey) === false, ARRAY_FILTER_USE_KEY);
     }
 
-    return parseAgnosticDsn($envs['DATABASE_URL'] ?? getenv('DATABASE_URL'));
+    return [
+        'dsn' => $envs['DATABASE_URL'] ?? getenv('DATABASE_URL'),
+    ];
 }
 
 $phinxDir = '%%PHINX_CONFIG_DIR%%';
